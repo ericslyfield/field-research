838d4600743d06b783bb0a237eb04d0a100f6092
Sly <eric.slyfield@gmail.com>
2025-09-28 17:38:56 -0400

    Initial Commit
    Adds default theme architecture

---
+++ src/ComponentOne.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ComponentOne(){
+    return(
+        <div className="test">
+            <h1>Title</h1>
+            <p>Line 1. Testing. Hello! </p>
+        </div>
+    )
+}
+
+export default ComponentOne
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import reactDOM from 'react-dom'
+
+import ComponentOne from './ComponentOne'
+
+reactDOM.render(<ComponentOne />, document.querySelector("#app"))
