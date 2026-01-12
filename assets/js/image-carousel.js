9aaeccb36e7c72a082a60033a1d6c2645b50787d
Sly <eric.slyfield@gmail.com>
2026-01-11 20:11:25 -0500

    Refined Carousel Gradient
    - Updates carousel controller and gradient styles

--- assets/js/image-carousel.js
+++ assets/js/image-carousel.js
@@ -1,41 +1,15 @@
-function imageCarousel(){
+const carousels = document.querySelectorAll('.carousel');
 
-    var target = document.getElementById("target");
-
-    var lastSlide = document.getElementById("last-slide-btn");
-    var nextSlide = document.getElementById("next-slide-btn");
-
-    if (lastSlide){
-        lastSlide.addEventListener("click", function(){
-            var getLastSlide = new XMLHttpRequest();
-            getLastSlide.open('GET', 'http://field-research.local/wp-json/wp/v2/posts');
-            getLastSlide.onLoad = function() {
-                if(getLastSlide.status >= 200 && getLastSlide.status < 400){
-                    var data = JSON.parse(getLastSlide.responseText);
-                } else {
-                    console.log("The server connected but returned an error...");
-                }
-            };
-
-            getLastSlide.onerror = function() {
-                console.log("Connection error");
-            }
-
-            getLastSlide.send();
-            
-        });
-    }
-
-        if (nextSlide){
-        nextSlide.addEventListener("click", function(){
-            console.log("Next slide button clicked!")
-        })
-    }
-
-    var lastPost = [];
-    var nextPost = [];
-
-    console.log("imageCarousel is Running.");
-}
-
-imageCarousel();
+carousels.forEach(carousel => {
+  const wrapper = carousel.closest('.carousel-wrapper');
+  
+  carousel.addEventListener('scroll', () => {
+    const isAtStart = carousel.scrollLeft <= 10;
+    const isAtEnd = carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth - 10;
+    
+    wrapper.classList.toggle('at-start', isAtStart);
+    wrapper.classList.toggle('at-end', isAtEnd);
+  });
+  
+  carousel.dispatchEvent(new Event('scroll'));
+});
