eaa8445a83b2af5ba39a4e25fba3f173d5602933
Sly <eric.slyfield@gmail.com>
2026-04-21 19:27:44 -0400

    feat: add extended tagline
    a tagline for landing pages

---
+++ patterns/extended-tagline.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Extended Tagline
+ * Slug: field-research/extended-tagline
+ * Description: A grouped, extended tagline for landing pages. This pattern uses the site tagline set in the users admin settings.
+ * Keywords: text, featured, header
+ * Categories: Featured, Text
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Extended Tagline"},"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"small","fontFamily":"rubik"} -->
+<p class="has-contrast-color has-text-color has-link-color has-rubik-font-family has-small-font-size">Vivamus sit amet finibus sem. In eu feugiat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:site-tagline {"style":{"elements":{"link":{"color":{"text":"var:preset|color|slate"}}}},"textColor":"slate","fontSize":"small"} /--></div>
+<!-- /wp:group -->
