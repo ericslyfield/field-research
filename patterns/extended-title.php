eb8d32d11313922a733b7968e3bc6dfa47abe362
Sly <eric.slyfield@gmail.com>
2026-04-21 19:39:16 -0400

    feat: add extented title
    - a tagline for secondary and tertiary pages
    - page title as breadcrumb

---
+++ patterns/extended-title.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Extended Title
+ * Slug: field-research/extended-title
+ * Description: An grouped, extended tagline for secondary pages. This pattern uses the page title to chart positioning. Especially useful on tertiary pages.
+ * Keywords: text, featured, header
+ * Categories: Featured, Text
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Extended Title"},"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"contrast","fontSize":"small","fontFamily":"rubik"} -->
+<p class="has-contrast-color has-text-color has-link-color has-rubik-font-family has-small-font-size">This page features selected works, and links to more work examples, organized by discipline.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"small"} /--></div>
+<!-- /wp:group -->
