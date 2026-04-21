9ae30d03ad981eafc81b56a791fefb96c0e4118e
Sly <eric.slyfield@gmail.com>
2026-04-21 19:20:31 -0400

    feat: default header
    Default header pattern (references text pattern)

---
+++ patterns/header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Header
+ * Slug: field-research/header
+ * Categories: header
+ * Description: Default Site Header. A sticky container 
+ * 
+ */
+?>
+
+<!-- wp:pattern {"slug":"field-research/header-with-text"} /-->
