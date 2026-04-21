5858faa51f3670394582b0639607461b873ab7c0
Sly <eric.slyfield@gmail.com>
2026-04-21 19:21:58 -0400

    feat: add header variations
    - header with site logo pattern
    - header with site title (text) pattern

---
+++ patterns/header-with-logo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Header with Site Logo
+ * Slug: field-research/header-with-logo
+ * Categories: header
+ * Description: Default Site Header. A sticky container 
+ * 
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Header Container"},"style":{"spacing":{"padding":{"right":"0","left":"0","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"center","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0"><!-- wp:group {"metadata":{"name":"Home Icon"},"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":20,"style":{"layout":{"selfStretch":"fixed","flexSize":"15px"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Navigation"},"style":{"position":{"type":""},"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:navigation {"ref":24,"textColor":"contrast","overlayMenu":"never","icon":"menu","layout":{"type":"flex","flexWrap":"nowrap"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/header-with-text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Header: Text
+ * Slug: field-research/header-with-text
+ * Categories: header
+ * Description: A default site header rendered in text.
+ * 
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Header Container"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Header Row"},"style":{"spacing":{"padding":{"right":"0","left":"0","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"center","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0"><!-- wp:site-title {"fontSize":"medium"} /-->
+
+<!-- wp:navigation {"ref":24,"textColor":"contrast","overlayMenu":"never","icon":"menu","fontSize":"medium","layout":{"type":"flex","flexWrap":"nowrap"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
