f73d488488282d27a55cd3ca7adba1f25749a813
Sly <eric.slyfield@gmail.com>
2026-04-21 19:25:52 -0400

    feat: add crossbar
    a fancy name for a page separator

---
+++ patterns/crossbar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Crossbar
+ * Slug: field-research/crossbar
+ * Description: A fancy name for a page separator that lives atop pages. Creates consistency while suggesting the visual divide between a header and the body content. Can be styled.
+ * Keywords: headers, banners,
+ * Categories: Banners, Headers
+ */
+?>
+
+<!-- wp:separator {"metadata":{"name":"Crossbar"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"contrast"} -->
+<hr class="wp-block-separator has-text-color has-contrast-color has-alpha-channel-opacity has-contrast-background-color has-background" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"/>
+<!-- /wp:separator -->
