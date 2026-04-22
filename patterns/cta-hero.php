078dca0e69b1ab88d35042baae75a08889430727
Sly <eric.slyfield@gmail.com>
2026-04-21 21:39:15 -0400

    feat: call-to-action hero with blur effect
    pre-styled CTA to blur all text apart from links

---
+++ patterns/cta-hero.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: CTA Hero
+ * Slug: field-research/cta-hero
+ * Description: A text-forward call-to-action doubling as site navigation.
+ * Keywords: text, featured, hero
+ * Categories: Featured, Text
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"CTA Hero"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"padding":{"right":"0","left":"0","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|50"},"dimensions":{"minHeight":"75vh"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group" style="min-height:75vh;margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60);padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0"><!-- wp:columns {"verticalAlignment":"bottom"} -->
+<div class="wp-block-columns are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:heading {"level":1,"className":"translate-up has-blur-effect","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"x-large","fontFamily":"shippori-antique-b1"} -->
+<h1 class="wp-block-heading translate-up has-blur-effect has-contrast-color has-text-color has-link-color has-shippori-antique-b-1-font-family has-x-large-font-size">This website <a href="http://field-research.local/log/" data-type="page" data-id="469">logs</a> information <a href="http://field-research.local/about/" data-type="page" data-id="254">about</a> the <a href="http://field-research.local/work/" data-type="page" data-id="764">work</a> of Eric Slyfield. Eric is also on <a href="http://www.linkedin.com/in/ericslyfield">LinkedIn</a>, <a href="http://instagram.com/ericslyfield">Instagram</a>, and <a href="https://www.are.na/eric-slyfield/channels">Are.na</a>.</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
