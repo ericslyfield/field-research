da74e584b4b598f70227bbeda3c9183681d22741
Sly <eric.slyfield@gmail.com>
2026-09-04 11:29:26 -0400

    add new header pattern
    adds a header wrapped in a group block with medium top padding

---
+++ patterns/header-with-top-padding.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Header w/ Top Padding
+ * Slug: field-research/header-with-top-padding
+ * Categories: header
+ * Description: A header, in a Group block, with a default Top Padding
+ * 
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Sticky Header with Top Padding"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}},"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:template-part {"slug":"header","theme":"field-research"} /--></div>
+<!-- /wp:group -->
