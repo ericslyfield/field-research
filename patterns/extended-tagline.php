dd94c3c9e0b805bd5b9e38376c6e42eabc892ac3
Sly <eric.slyfield@gmail.com>
2026-09-05 23:35:56 -0400

    update extended-tagline.php
    changes text size to small. changes tagline font to rubik.

--- patterns/extended-tagline.php
+++ patterns/extended-tagline.php
@@ -2,16 +2,16 @@
 /**
  * Title: Extended Tagline
  * Slug: field-research/extended-tagline
- * Description: A grouped, extended tagline for landing pages. This pattern uses the site tagline set in the users admin settings.
- * Keywords: text, featured, header
+ * Description: A grouped, extended tagline for landing pages. This pattern dynamically references the site tagline set in the users admin settings.
+ * Keywords: text, featured, header, tagline
  * Categories: Featured, Text
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Extended Tagline"},"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"small","fontFamily":"rubik"} -->
-<p class="has-contrast-color has-text-color has-link-color has-rubik-font-family has-small-font-size">Vivamus sit amet finibus sem. In eu feugiat.</p>
+<!-- wp:group {"metadata":{"name":"Extended Tagline"},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|slate"}}}},"textColor":"slate","fontSize":"x-small","fontFamily":"rubik"} -->
+<p class="has-slate-color has-text-color has-link-color has-rubik-font-family has-x-small-font-size">Another WordPress Theme</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:site-tagline {"style":{"elements":{"link":{"color":{"text":"var:preset|color|slate"}}}},"textColor":"slate","fontSize":"small"} /--></div>
+<!-- wp:site-tagline {"style":{"elements":{"link":{"color":{"text":"var:preset|color|slate"}}},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"slate","fontSize":"x-small","fontFamily":"rubik"} /--></div>
 <!-- /wp:group -->
